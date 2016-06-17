4661828cd717a33b0f18adaa4f12dc84b94649e7
scarselli <scarselli@gmail.com>
2016-06-17 19:24:01 +0200

    now library is importable

--- python_geojson_pandas_join.py
+++ python_geojson_pandas_join.py
@@ -5,7 +5,8 @@ import pandas as pd
 import json
 
 
-def join(geojson, excel_dataframe, key_geojson, key_pandas, output, missing_columns=0, missing_rows=0):
+
+def join(geojson, excel_dataframe, key_geojson, key_pandas, output, missing_columns=0, missing_rows=0, type_dataframe = False):
     """ this library add 'columns' to geojson from 'excel' well structured file
     :param geojson: geojson build with qgis (url)
     :param excel_dataframe: simple excel without multindex and monodimensional (url)
@@ -16,11 +17,15 @@ def join(geojson, excel_dataframe, key_geojson, key_pandas, output, missing_colu
     :param missing_rows: number of cell with NAN value for row to drop
     :return: none
     """
+    if type_dataframe == False:
+        dataframe = pd.read_excel(excel_dataframe)
+    else:
+        dataframe = excel_dataframe
 
-    dataframe = pd.read_excel(excel_dataframe)
     # geojson doesn't like nan!
     dataframe.dropna(inplace=True, axis=1, thresh=missing_columns)
     dataframe.dropna(inplace=True, axis=0, thresh=missing_rows)
+    dataframe.fillna(0, inplace = True)
 
     with open(geojson) as geojson_code:
         geojson_dict = json.load(geojson_code)
@@ -34,38 +39,10 @@ def join(geojson, excel_dataframe, key_geojson, key_pandas, output, missing_colu
     with open(output, "w") as outfile:
         json.dump(geojson_dict, outfile)
 
-    return "executed " + geojson + " " + excel_dataframe
-
-
-def join_df(geojson, dataframe, key_geojson, key_pandas, output, missing_columns=0, missing_rows=0):
-    """ this library add 'columns' to geojson from 'pandas'
-    :param geojson: geojson build with qgis (url)
-    :param dataframe: simple excel without multindex and monodimensional (url)
-    :param key_geojson: key column
-    :param key_pandas: key column
-    :param output: url
-    :param missing_columns: number of cell with NAN value for row to drop
-    :param missing_rows: number of cell with NAN value for row to drop
-    :return: none
-    """
-    # geojson doesn't like nan!
-    dataframe.dropna(inplace=True, axis=1, thresh=missing_columns)
-    dataframe.dropna(inplace=True, axis=0, thresh=missing_rows)
-
-    with open(geojson) as geojson_code:
-        geojson_dict = json.load(geojson_code)
-
-    for row in geojson_dict["features"]:
-        dict_temporary_data = dataframe[dataframe[key_pandas] == row[u'properties'][key_geojson]] \
-            .to_dict(orient="records")
-        if len(dict_temporary_data) > 0:
-            row[u'properties'].update(dict_temporary_data[0])
-
-    with open(output, "w") as outfile:
-        json.dump(geojson_dict, outfile)
-
-    return "executed " + geojson + " pandas dataframe"
-
+    if type_dataframe == False:
+        return "executed " + geojson + " " + excel_dataframe
+    else:
+        return "executed " + geojson + " " + "dataframe"
 
 if __name__ == "__main__":
     # link_test_geojson = r"E:\Dropbox\Dropbox\0_DB\0_SDMX\nuts2.json"
